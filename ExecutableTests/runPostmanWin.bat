86a58776ebabecc0a6cce5a506d19a7ecdee5577
IvanPetkovA5 <133020617+IvanPetkovA5@users.noreply.github.com>
2023-10-19 23:18:03 +0300

    Update runPostmanWin.bat

--- ExecutableTests/runPostmanWin.bat
+++ ExecutableTests/runPostmanWin.bat
@@ -1,2 +1,2 @@
 REM Run Postman Collection using Newman
-newman run your-collection.json
+newman run https://raw.githubusercontent.com/TEAM-1-A50/WEare-API-Postman/main/postman/collections/API%20testing.json
